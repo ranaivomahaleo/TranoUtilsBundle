7f7421f1df2cf6be2048fe87ac338f09d142d9f4
RAZAKANIRINA <ranaivo.razakanirina@atety.com>
2021-02-20 12:28:44 +0100

    Add headers to increase web security.

--- Util/ApiJsonResponse.php
+++ Util/ApiJsonResponse.php
@@ -45,6 +45,50 @@ class ApiJsonResponse
     private function buildAccessControlHeaders()
     {
         $headers = [];
+
+        // Always set to insure HSTS
+        $headers['Strict-Transport-Security'] = 'max-age=31536000M';
+        if ($this->env->getEnv('HEADER_STRICT_TRANSPORT_SECURITY')) {
+            $headers['Strict-Transport-Security'] = $this->env->getEnv('HEADER_STRICT_TRANSPORT_SECURITY');
+        }  // if
+
+        // Set to no-store by default to avoid storing information in the cache of the browser
+        $headers['Cache-Control'] = 'no-store';
+        if ($this->env->getEnv('HEADER_CACHE_CONTROL')) {
+            $headers['Cache-Control'] = $this->env->getEnv('HEADER_CACHE_CONTROL');
+        }  // if
+
+        // Do not include a referrer info by default to avoid tracking
+        $headers['Referrer-Policy'] = 'no-referrer';
+        if ($this->env->getEnv('HEADER_REFERRER_POLICY')) {
+            $headers['Referrer-Policy'] = $this->env->getEnv('HEADER_REFERRER_POLICY');
+        }  // if
+
+        // Enforce the MIME type related to the browser by default
+        $headers['X-Content-Type-Options'] = 'nosniff';
+        if ($this->env->getEnv('HEADER_X_CONTENT_TYPE_OPTIONS')) {
+            $headers['X-Content-Type-Options'] = $this->env->getEnv('HEADER_X_CONTENT_TYPE_OPTIONS');
+        }  // if
+
+        // Load resources types from the own domain and same origin. Wildcard may be used such as *.example.com
+        $headers['Content-Security-Policy'] = "default-src 'self'";
+        if ($this->env->getEnv('HEADER_CONTENT_SECURITY_POLICY')) {
+            $headers['Content-Security-Policy'] = $this->env->getEnv('HEADER_CONTENT_SECURITY_POLICY');
+        }  // if
+
+        // Deny framing by default
+        $headers['X-Frame-Options'] = "deny";
+        if ($this->env->getEnv('HEADER_X_FRAME_OPTIONS')) {
+            $headers['X-Frame-Options'] = $this->env->getEnv('HEADER_X_FRAME_OPTIONS');
+        }  // if
+
+        // Configure by default the browser with XSS filters
+        $headers['X-XXS-Protection'] = "1; mode=block";
+        if ($this->env->getEnv('HEADER_X_XXS_PROTECTION')) {
+            $headers['X-XXS-Protection'] = $this->env->getEnv('HEADER_X_XXS_PROTECTION');
+        }  // if
+
+
         if ($this->env->getEnv('ALLOWED_ORIGIN')) {
             $headers['Access-Control-Allow-Origin'] = $this->env->getEnv('ALLOWED_ORIGIN');
         } // if
